9357dc09abe3ca2089215b4ec8b136ea6fe290ef
Tejas Adhikari <tejasadhikari123@gmail.com>
2024-11-20 22:13:12 -0500

    a6 Initial commit

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -6,27 +6,36 @@ export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
+
   return (
-    <div id="wd-account-navigation"  className="wd list-group fs-5 rounded-0">
-      <NavLink
-        id="wd-account-signin-link" to="/Kanbas/Account/Signin"
-        className={({ isActive }) => `list-group-item border border-0 ${
-          isActive ? "active" : "text-danger"}`}>
-        Signin
-      </NavLink>
-      <NavLink
-        id="wd-account-signup-link" to="/Kanbas/Account/Signup"
-        className={({ isActive }) => `list-group-item border border-0 ${
-          isActive ? "active" : "text-danger"}`}>
-        Signup
-      </NavLink>
-      {/* <ProtectedRoute>  */}
-      <NavLink
-        id="wd-account-profile-link" to="/Kanbas/Account/Profile"
-        className={({ isActive }) => `list-group-item border border-0 ${
-          isActive ? "active" : "text-danger"}`}>
-        Profile
-      </NavLink>
-      {/* </ProtectedRoute>  */}
-    </div>
+    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+     {links.map((link) => (
+       <NavLink key={link} to={`/Kanbas/Account/${link}`} 
+        className={`list-group-item ${active(link)}`} > {link} </NavLink>
+     ))}
+     {currentUser && currentUser.role === "ADMIN" && (
+       <NavLink to={`/Kanbas/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </NavLink> )}
+   </div>
+
+    // <div id="wd-account-navigation"  className="wd list-group fs-5 rounded-0">
+    //   <NavLink
+    //     id="wd-account-signin-link" to="/Kanbas/Account/Signin"
+    //     className={({ isActive }) => `list-group-item border border-0 ${
+    //       isActive ? "active" : "text-danger"}`}>
+    //     Signin
+    //   </NavLink>
+    //   <NavLink
+    //     id="wd-account-signup-link" to="/Kanbas/Account/Signup"
+    //     className={({ isActive }) => `list-group-item border border-0 ${
+    //       isActive ? "active" : "text-danger"}`}>
+    //     Signup
+    //   </NavLink>
+    //   <NavLink
+    //     id="wd-account-profile-link" to="/Kanbas/Account/Profile"
+    //     className={({ isActive }) => `list-group-item border border-0 ${
+    //       isActive ? "active" : "text-danger"}`}>
+    //     Profile
+    //   </NavLink> 
+    // </div>
 );}
--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,53 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import * as client from "./client";
 import { useDispatch, useSelector } from "react-redux";
 import { addPerson, setPeople, deletePerson } from "./reducer";
 import ProtectedEdit from "../../Account/ProtectedEdit";
 import PeopleControls from "./PeopleControls";
 import { FaTrash } from "react-icons/fa6";
+import PeopleDetails from "./Details";
+import { Link } from "react-router-dom";
 
-export default function PeopleTable () {
-  const { cid } = useParams();
+export default function PeopleTable ({ users = [] }: { users?: any[] }) {
+  // const { cid } = useParams();
   // const { users, enrollments } = db;
-  const { enrollments } = useSelector((state: any) => state.peopleReducer);
+  // const { enrollments } = useSelector((state: any) => state.peopleReducer);
   const [personId, setPersonId] = useState("");
 
   const dispatch = useDispatch();
 
-  const fetchPeople = async () => {
-    const people = await client.fetchAllPeople(cid as string);
-    // console.log(people);
-    dispatch(setPeople(people));
-  };
+  // const fetchPeople = async () => {
+  //   const people = await client.fetchAllPeople(cid as string);
+  //   // console.log(people);
+  //   dispatch(setPeople(people));
+  // };
 
-  const createEntryForPerson = async () => {
-    if (!cid) return;
-    // const newModule = { name: personId, course: cid };
-    const people = await client.enrollPersonInCourse(cid, personId);
-    dispatch(addPerson(people));
-    dispatch(setPeople(people));
-  };
+  // const createEntryForPerson = async () => {
+  //   if (!cid) return;
+  //   // const newModule = { name: personId, course: cid };
+  //   const people = await client.enrollPersonInCourse(cid, personId);
+  //   dispatch(addPerson(people));
+  //   dispatch(setPeople(people));
+  // };
 
-  const deleteEntryForPerson = async (user: any) => {
-    console.log(user._id);
-    const newPeople = await client.unenrollPersonFromCourse(cid, user._id);
-    // console.log("Dashboard courses response: ", newCourses);
-    dispatch(deletePerson({user: user._id, course: cid}));
-    dispatch(setPeople(newPeople));
-  }
+  // const deleteEntryForPerson = async (user: any) => {
+  //   console.log(user._id);
+  //   const newPeople = await client.unenrollPersonFromCourse(cid, user._id);
+  //   // console.log("Dashboard courses response: ", newCourses);
+  //   dispatch(deletePerson({user: user._id, course: cid}));
+  //   dispatch(setPeople(newPeople));
+  // }
 
-  useEffect(() => {
-    fetchPeople();
-  }, []);
+  // useEffect(() => {
+  //   fetchPeople();
+  // }, []);
 
   return (
     <div id="wd-people-table">
-
-      <ProtectedEdit>
+      <PeopleDetails />
+      {/* <ProtectedEdit>
         <PeopleControls personId={personId} setPersonId={setPersonId} addPerson={createEntryForPerson}/>
-      </ProtectedEdit> 
+      </ProtectedEdit>  */}
       <br /><br /><br /><br />
     
       <table className="table table-striped">
@@ -57,12 +59,14 @@ export default function PeopleTable () {
           </tr>
         </thead>
         <tbody>
-        {enrollments.map((user: any) => (
+        {users.map((user: any) => (
             <tr key={user._id}>
               <td className="wd-full-name text-nowrap">
-                <FaUserCircle className="me-2 fs-1 text-secondary" />
-                <span className="wd-first-name">{user.firstName}</span>
-                <span className="wd-last-name">{user.lastName}</span>
+                <Link to={`/Kanbas/Account/Users/${user._id}`} className="text-decoration-none">
+                  <FaUserCircle className="me-2 fs-1 text-secondary" />
+                  <span className="wd-first-name">{user.firstName}</span>
+                  <span className="wd-last-name">{user.lastName}</span>
+                </Link>
               </td>
               <td className="wd-login-id">{user.loginId}</td>
               <td className="wd-section">{user.section}</td>
@@ -70,9 +74,9 @@ export default function PeopleTable () {
               <td className="wd-last-activity">{user.lastActivity}</td>
               <td className="wd-total-activity">{user.totalActivity}</td>
               <td className="wd-delete">
-                <ProtectedEdit>
+                {/* <ProtectedEdit>
                   <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteEntryForPerson(user)}/>
-                </ProtectedEdit>
+                </ProtectedEdit> */}
               </td>
             </tr>
           ))}
